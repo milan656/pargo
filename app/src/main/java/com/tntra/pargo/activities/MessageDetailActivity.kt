93290e956d8c12b6da7e839ebe4fb3a5c0bb2d16
Subhash Prajapati <subhash.prajapati@techifysolutions.com>
2021-08-14 12:05:08 +0530

    "pargo app"

--- app/src/main/java/com/tntra/pargo/activities/MessageDetailActivity.kt
+++ app/src/main/java/com/tntra/pargo/activities/MessageDetailActivity.kt
@@ -74,7 +74,6 @@ class MessageDetailActivity : AppCompatActivity(), View.OnClickListener {
         mSocket?.on("updateChat", onUpdateChat) // To update if someone send a message to chatroom
         mSocket?.on("userLeftChatRoom", onUserLeft)
 
-
         initView()
     }
 
